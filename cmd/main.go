5cbd6d4cd63c7fbd26d3989ccca360633ded1d80
Daniel Teunis <daniel@nyarlathotep.local>
2018-09-14 15:53:52 +0200

    More senseful connection handling

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"../gethTest"
 	"fmt"
-	"net"
+	"time"
 )
 
 func main() {
@@ -14,28 +14,15 @@ func main() {
 	// IP + port of our own geth instance
 	gethIp := "127.0.0.1"
 	gethPort := "30302"
-	// Port of this geth-test instanceS
-	localIp := ""
 	// private key file
 	privKeyFile := "keys/priv1"
 
-	// get local address
-	c, err := net.Dial("udp", net.JoinHostPort(targetIp, targetPort))
-	if err != nil {
-		fmt.Println("Target seems to be offline (", err, ")")
-	}
-	localIp, _, _ = net.SplitHostPort(c.LocalAddr().String())
-	c.Close()
+	s := gethTest.NewPingServer(targetIp, targetPort, gethIp, gethPort)
 
-	s := gethTest.PingServer{
-		TargetIp:   targetIp,
-		TargetPort: targetPort,
-		GethIp:     gethIp,
-		GethPort:   gethPort,
-		LocalIp:    localIp,
-	}
 	s.ParseKeyFile(privKeyFile)
-	s.StartPingLoop()
+	s.Start()
+	time.Sleep(10 * time.Second)
+	s.Stop()
 
 	return
 }
--- gethTest/pingserver.go
+++ gethTest/pingserver.go
@@ -6,34 +6,43 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
 	"fmt"
 	"net"
 	"os"
 	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
-type PingServer struct {
-	TargetIp   string
-	TargetPort string
-	GethIp     string
-	GethPort   string
-	LocalIp    string
+type pingServer struct {
+	targetIp   string
+	targetPort string
+	gethIp     string
+	gethPort   string
+	localIp    string
 	privKey    string
-	conn       net.UDPConn
+
+	macSize int
+	sigSize int
+
+	conn    net.UDPConn
+	closing chan struct{}
 }
 
 // private functions
-func (s PingServer) ping() {
+func (s pingServer) ping() {
 	// create ping packet
 	theirIP := net.IPv4(127, 0, 0, 1)
-	tcpPort := uint16(30303)
+	tcpPort := uint16(30309)
 	expiration := 20 * time.Second
 	addr := &net.UDPAddr{
 		IP:   theirIP,
-		Port: 30303,
+		Port: 30309,
 	}
 	toaddr := &net.UDPAddr{
 		IP:   theirIP,
@@ -47,10 +56,8 @@ func (s PingServer) ping() {
 		Expiration: uint64(time.Now().Add(expiration).Unix()),
 	}
 
-	macSize := 256 / 8
-	sigSize := 520 / 8
 	ptype := byte(1)
-	headSize := macSize + sigSize
+	headSize := s.macSize + s.sigSize
 	headSpace := make([]byte, headSize)
 
 	b := new(bytes.Buffer)
@@ -65,20 +72,26 @@ func (s PingServer) ping() {
 
 	// create new private key
 	// TODO: use own private key
-	ellc := elliptic.P256()
+	ellc := secp256k1.S256()
 	priv, err := ecdsa.GenerateKey(ellc, rand.Reader)
 	if err != nil {
 		fmt.Println("Can't generate key (", err, ")")
 	}
+	pubkey := elliptic.Marshal(ellc, priv.X, priv.Y)
+	privkey := make([]byte, 32)
+	blob := priv.D.Bytes()
+	copy(privkey[32-len(blob):], blob)
+	fmt.Println("=== prikey:", privkey)
+	fmt.Println("=== pubkey:", pubkey)
 
 	sig, err := crypto.Sign(crypto.Keccak256(packet[headSize:]), priv)
 	if err != nil {
 		fmt.Println("Can't sign discv4 packet (", err, ")")
 	}
-	copy(packet[macSize:], sig)
+	copy(packet[s.macSize:], sig)
 	//fmt.Println(packet)
 
-	hash := crypto.Keccak256(packet[macSize:])
+	hash := crypto.Keccak256(packet[s.macSize:])
 	copy(packet, hash)
 	//fmt.Println(packet)
 
@@ -87,79 +100,92 @@ func (s PingServer) ping() {
 	if err != nil {
 		fmt.Println("Error sending ping (", err, ")")
 	}
+
+	pub := priv.Public()
+	x509EncodedPub, _ := x509.MarshalPKIXPublicKey(pub)
+	pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY",
+		Bytes: x509EncodedPub})
+
+	fmt.Println("Public Key:", pemEncodedPub)
+	fmt.Println("Public Key:", x509EncodedPub)
 }
 
-func (s PingServer) pingLoop() {
-	// open connection to target
-	addr := net.UDPAddr{
-		Port: 30302,
-		IP:   net.ParseIP("127.0.0.1"),
-	}
-	conn, err := net.ListenUDP("udp", &addr)
+func (s pingServer) receive() {
+	headSize := s.macSize + s.sigSize
+
+	inBuf := make([]byte, 1280)
+	s.conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	readLen, _, err := s.conn.ReadFromUDP(inBuf)
 	if err != nil {
-		fmt.Println("Target seems to be offline (", err, ")")
+		fmt.Println("Error receiving pong (", err, ")")
+		return
 	}
-	defer conn.Close()
-	s.conn = *conn
+	inBuf = inBuf[:readLen]
+	if len(inBuf) < headSize+1 {
+		fmt.Println("Packet too small (", err, ")")
+	}
+	hash, sig, sigdata := inBuf[:s.macSize], inBuf[s.macSize:headSize], inBuf[headSize:]
+	shouldhash := crypto.Keccak256(inBuf[s.macSize:])
+	if !bytes.Equal(hash, shouldhash) {
+		fmt.Println("Wrong hash!")
+		fmt.Println("Hash:", hash)
+		fmt.Println("Should Hash:", shouldhash)
+	}
+	fromID, err := recoverNodeID(crypto.Keccak256(inBuf[headSize:]), sig)
+	if err != nil {
+		fmt.Println("Failed to recover node (", err, ")")
+	}
+	fmt.Println("FromID:", fromID)
+	// sigdata[0]:
+	// x01 -> ping
+	// x02 -> pong
+	// x03 -> findnode
+	// x04 -> neighbors
 
-	// this should become a ping loop in the long run
-	go s.receiveLoop()
-	go s.ping()
+	// for now: ignore all but pong packets
+	if sigdata[0] != byte(2) {
+		return
+	}
+
+	largeArray := make([]byte, len(fromID))
+	copy(largeArray[:], fromID[:])
+
+	req := new(pong)
+	sd := rlp.NewStream(bytes.NewReader(sigdata[1:]), 0)
+	err = sd.Decode(req)
+	fmt.Println(largeArray)
+	fmt.Printf("%s", hex.Dump(largeArray))
+}
+
+func (s pingServer) pingLoop() {
+	fmt.Println("Starting Ping Loop...")
 	for {
+		select {
+		case <-s.closing:
+			fmt.Println("Stopping Ping Loop...")
+			return
+		default:
+			s.ping()
+			time.Sleep(1 * time.Second)
+		}
 	}
 }
 
-func (s PingServer) receiveLoop() {
-	macSize := 256 / 8
-	sigSize := 520 / 8
-	headSize := macSize + sigSize
+func (s pingServer) receiveLoop() {
+	fmt.Println("Starting Receive Loop...")
 	for {
-		inBuf := make([]byte, 1280)
-		readLen, _, err := s.conn.ReadFromUDP(inBuf)
-		if err != nil {
-			fmt.Println("Error receiving pong (", err, ")")
-		}
-		inBuf = inBuf[:readLen]
-		fmt.Println("Received Package!")
-		fmt.Println("length:", readLen)
-		fmt.Println("received:", inBuf)
-		if len(inBuf) < headSize+1 {
-			fmt.Println("Packet too small (", err, ")")
-		}
-		hash, sig, sigdata := inBuf[:macSize], inBuf[macSize:headSize], inBuf[headSize:]
-		shouldhash := crypto.Keccak256(inBuf[macSize:])
-		if !bytes.Equal(hash, shouldhash) {
-			fmt.Println("Wrong hash!")
-			fmt.Println("Hash:", hash)
-			fmt.Println("Should Hash:", shouldhash)
-		}
-		fromID, err := recoverNodeID(crypto.Keccak256(inBuf[headSize:]), sig)
-		if err != nil {
-			fmt.Println("Failed to recover node (", err, ")")
-		}
-		fmt.Println("FromID:", fromID)
-		fmt.Println("sigdata[0]:", sigdata[0])
-		fmt.Println(" ")
-		// sigdata[0]:
-		// x01 -> ping
-		// x02 -> pong
-		// x03 -> findnode
-		// x04 -> neighbors
-
-		// for now: ignore all but pong packets
-		if sigdata[0] != byte(2) {
-			continue
-		} else {
-			fmt.Println("Received Pong Packet")
-			req := new(pong)
-			sd := rlp.NewStream(bytes.NewReader(sigdata[1:]), 0)
-			err = sd.Decode(req)
+		select {
+		case <-s.closing:
+			fmt.Println("Stopping Receive Loop...")
+			return
+		default:
+			s.receive()
 		}
 	}
 }
 
 // public functions
-func (s PingServer) ParseKeyFile(privKeyFile string) {
+func (s pingServer) ParseKeyFile(privKeyFile string) {
 	f, err := os.Open(privKeyFile)
 	if err != nil {
 		fmt.Println("Key file is missing. (", err, ")")
@@ -174,12 +200,41 @@ func (s PingServer) ParseKeyFile(privKeyFile string) {
 
 }
 
-func (s PingServer) StartPingLoop() {
-	s.pingLoop()
+func (s pingServer) Start() {
+	go s.receiveLoop()
+	go s.pingLoop()
 }
 
-func (s PingServer) StopPingLoop() {}
+func (s pingServer) Stop() {
+	close(s.closing)
+	time.Sleep(5 * time.Second)
+	fmt.Println("Closing Connection...")
+	s.conn.Close()
+}
 
-func (s PingServer) PrintData() {
-	fmt.Println("foobar")
+func NewPingServer(tIp, tPort, gIp, gPort string) *pingServer {
+	s := new(pingServer)
+	s.targetIp = tIp
+	s.targetPort = tPort
+	s.gethIp = gIp
+	s.gethPort = gPort
+	s.closing = make(chan struct{})
+
+	s.macSize = 256 / 8
+	s.sigSize = 520 / 8
+
+	// open connection to target
+	addr := net.UDPAddr{
+		Port: 30309,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Println("Target seems to be offline (", err, ")")
+	}
+	localIp, _, _ := net.SplitHostPort(conn.LocalAddr().String())
+	s.localIp = localIp
+	s.conn = *conn
+
+	return s
 }
